caab1b628dbc4e443827ab79571ea46dfea2efa0
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-12 17:42:49 +0200

    primer commit

---
+++ app/src/main/java/com/example/promul_practica_scroll/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.promul_practica_scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
