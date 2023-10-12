37bfaef43aca7977d88e513f56a57e3890320d22
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-12 18:12:48 +0200

    primer commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.promul_practica_scroll"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.promul_practica_scroll"
--- app/src/main/java/com/example/promul_practica_scroll/MainActivity.java
+++ app/src/main/java/com/example/promul_practica_scroll/MainActivity.java
@@ -6,6 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /*
+    * Para crear un scroll vertical, debemos agregar el elemento Containers -> ScrollView.
+    *
+    * Hay que tener en cuenta, que el elemento ScrollView solo puede tener un hijo directo,
+    * así que si queremos que varios elementos estén dentro del contenedor ScrollView,
+    * debemos primero ponerlos en un layout (agregando un Layouts -> ConstraintLayout, LinearLayout, o el que sea),
+    * y seguidamente, poner ese layout dentro del ScrollView.
+    *
+    * En este ejemplo, a sido un texto principal fuera del scroll, luego un ScrollView
+    * que contiene un LinearLayout que a su vez contiene dos textos, uno que es el
+    * título secundario y otro que es el cuerpo del texto.
+    *
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
